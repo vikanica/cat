82fc3da30ba68073d13f0270dc94d0459993a0fe
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-04 12:31:55 +0300

    srykdtuocto

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = $app->add(['Button','click']);
+$button = $app->add(['Button','Whatsapp']);
 $button->icon = 'whatsapp';
 $button->link('https://web.whatsapp.com/');
