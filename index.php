81b6e7bcb51b7b438be1d487ae4db6a33cbde19c
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-06 15:58:56 +0300

    dht

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?PHP
-
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$button = $app->add(['Button','Whatsapp']);
-$button->icon = 'whatsapp';
-$button->link('https://web.whatsapp.com/');
+$button = $app->add(['Label','cat']);
+/*Class Cat {
+public $age;
+public $pol;
+public $name;
+public $color = 'White' ;
+const legs = 4;
+public function Birthday($year){
+$age1= $year- $this->age;
+return $age1;
+}
+public function WheelCount(){
+  return self: :Wheels ;
+}
+} */
