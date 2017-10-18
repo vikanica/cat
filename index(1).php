7cb316c8e6de56f99403c47894a710681d987b66
vikanica <32392416+vikanica@users.noreply.github.com>
2017-10-18 12:03:20 +0300

    dgjl

---
+++ index(1).php
@@ -0,0 +1,26 @@
+<?PHP
+require 'vendor/autoload.php';
+$app = new \atk4\ui\App('Registration');
+$app->initLayout('Centered');
+$label = $app->add(['Label','cat']);
+$text = $app->add(['text','cat']);
+$message = $app->add(['message','cat']);
+$button = $app->add(['button','cat']);
+$img = 'http://www.catster.com/wp-content/uploads/2017/08/A-brown-cat-licking-its-lips.jpg';
+$icon = $app->add(['Image', $img, 'disabled']);
+$button = $app->add(['Label', 'cat', 'image'=>$img]);
+$button->set(['massive'=>true]);
+/*Class Cat {
+public $age;
+public $pol;
+public $name;
+public $color = 'White' ;
+const legs = 4;
+public function Birthday($year){
+$age1= $year- $this->age;
+return $age1;
+}
+public function WheelCount(){
+  return self: :Wheels ;
+}
+} */
--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?PHP
+
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-$label = $app->add(['Label','cat']);
-$text = $app->add(['text','cat']);
-$message = $app->add(['message','cat']);
-$button = $app->add(['button','cat']);
-$img = 'http://www.catster.com/wp-content/uploads/2017/08/A-brown-cat-licking-its-lips.jpg';
-$icon = $app->add(['Image', $img, 'disabled']);
-$button = $app->add(['Label', 'cat', 'image'=>$img]);
-$button->set(['massive'=>true]);
-/*Class Cat {
-public $age;
-public $pol;
-public $name;
-public $color = 'White' ;
-const legs = 4;
-public function Birthday($year){
-$age1= $year- $this->age;
-return $age1;
-}
-public function WheelCount(){
-  return self: :Wheels ;
-}
-} */
+$button = $app->layout->add(['Button','link']);
+$button->link('text.txt');
