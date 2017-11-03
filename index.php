0e9f56c43d24e54b4ee858a5939cc3bccec6e9e9
vikanica <32392416+vikanica@users.noreply.github.com>
2017-11-03 14:38:34 +0200

    qwe

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<?PHP
-
+<?php
+echo "O privet";
 require 'vendor/autoload.php';
-$app = new \atk4\ui\App('Registration');
-$app->initLayout('Centered');
-$button = $app->layout->add(['Button','link']);
-$button->link('text.txt');
+$app = new \atk4\ui\App("Добро пожаловать");
+$app->initLayout("Centered");
+/*
+$button = $app->add('Button');
+$button->set('Add invoice');
+$button->icon = 'plus';
+$button->link('https://www.youtube.com/');
 
-$button = $app->layout->add(['Button','link']);
-$button->link(['index(1)','phone'=>'cdguk','number'=>6]);
+$button = $app->add('Button');
+$button->set('Add invoice');
+$button->icon = 'plus';
+$button->link('https://www.facebook.com/');
+
+$button = $app->add('Button');
+$button->set('Add invoice');
+$button->icon = 'plus';
+$button->link('https://www.e-klase.lv/lv/');
+*/
+$button= $app->add(['Button','Ilja']);
+$button->link('index2.php');
+$button->addClass('Massive Blue');
